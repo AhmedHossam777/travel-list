6f767f9f187bff9ac450d71d52fd8311b39ae4de
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-09-11 07:56:14 +0300

    add packing functoinality

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+
 const Item = ({ item, handleDeleteItem }) => {
+	const [checked, setChecked] = useState(item.packed);
 	return (
 		<li>
-			<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={(e) => setChecked(e.target.checked)}
+			/>
+
+			<span style={checked ? { textDecoration: 'line-through' } : {}}>
 				{item.quantity} {item.description}
 			</span>
+
 			<button onClick={() => handleDeleteItem(item.id)}>❌</button>
 		</li>
 	);
